e96114b95dc576cdf7c46bbb4ec61a610ec0f768
BAmine <amine.benhalloum@gmail.com>
2015-06-23 19:52:13 +0200

    shuffle before converting to vw format

--- cdiscount/run.bat
+++ cdiscount/run.bat
@@ -1,5 +1,6 @@
 SET VW_PATH=C:\src\vw\vowpal_wabbit\vowpalwabbit\x64\Debug
-pypy to_vw_format.py ../data/training.csv training.vw train Categorie3
+pypy shuffle.py ../data/training.csv ../data/training.shuffled.csv 1 1234
+pypy to_vw_format.py ../data/training.shuffled.csv training.vw train Categorie3
 pypy to_vw_format.py ../data/test.csv test.vw test Categorie3
 "%VW_PATH%\vw.exe" training.vw -f model.vw --loss_function logistic -b 21 -P 1000 --oaa 5786
 "%VW_PATH%\vw.exe" -t test.vw -i model.vw --loss_function logistic -b 21 -P 1000 -p preds.txt
